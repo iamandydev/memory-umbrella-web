73afa216ba50271e244318f58e1afb6ecfb0f566
iamandydev <andydev.me@outlook.com>
2025-11-10 22:20:01 -0500

    correciones

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,8 +4,14 @@ const router = Router();
 router.get("/", (req, res) => {
     res.render("index");
 });
+router.get("/productos", (req, res) => {
+    res.render("products-list");
+});
 router.get("/download", (req, res) => {
     res.render("download");
+});
+router.get("/Error", (req, res) => {
+    res.render("error");
 });
 
 export default router;
